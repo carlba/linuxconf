408670f304b366c829876b2ad122f212ba29adbd
carlba <genzorg@gmail.com>
2012-07-03 01:23:01 +0200

    Updated default freenode username

---
+++ git_configuration.sh
@@ -0,0 +1,10 @@
+su cada
+yum -y install git
+git config --global user.name "carlba"
+git config --global user.email "genzorg@gmail.com"
+echo "==== Git Configuration ===="
+git config --list
+cd ~
+git init
+git remote add origin https://github.com/carlba/linuxconf.git
+git pull

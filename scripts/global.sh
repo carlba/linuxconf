4e8dd6654a972f50ca0b18c624a6d467594abacf
Carl Bäckström <cada@mint>
2013-03-10 01:27:38 +0100

    Removed debug lines.

--- scripts/global.sh
+++ scripts/global.sh
@@ -3,10 +3,8 @@ function instrFile() {
   local needle="$1" 
   local filepath="$2"
   if grep -q "$needle" $filepath; then
-    echo "test"
     return 0
   else
-    echo "test2"
     return 1
   fi
 }

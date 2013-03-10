ffbb1f66802859a6c0b51c22ef3cd0384ca0720d
Carl Bäckström <cada@mint>
2013-03-10 01:25:57 +0100

    Added the md5gen function. Generates a md5hash takes a string as input.

--- scripts/global.sh
+++ scripts/global.sh
@@ -3,8 +3,14 @@ function instrFile() {
   local needle="$1" 
   local filepath="$2"
   if grep -q "$needle" $filepath; then
+    echo "test"
     return 0
   else
+    echo "test2"
     return 1
   fi
+}
+
+function md5gen() {
+  echo -n "$1" | md5sum
 }

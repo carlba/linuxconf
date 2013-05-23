90276c122ae5965f06b823d375dcf4b3e26737f9
Carl Bäckström <cada@mint>
2013-05-23 14:01:23 +0200

    Synergy is restarterd on autoscreen.

--- scripts/restartsynergy.sh
+++ scripts/restartsynergy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-source ~/dotfiles/bashrc.d/global.sh
+source $HOME/dotfiles/bashrc.d/global.sh
 
 if isHostOnline burken; then    
   pkill synergyc
   synergyc burken
+  [[ $- == *i* ]] && echo Synergy has been restarted.
+  ~/dotfiles/scripts/autoscreen.sh
+fi
+
+if isHostOnline marvin.birdstep.internal; then
+  pkill synergyc
+  synergyc wincada.d.birdstep.internal
   [[ $- == *i* ]] && echo Synergy has been restarted.
   ~/dotfiles/scripts/autoscreen.sh
 fi  

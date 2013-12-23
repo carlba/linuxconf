36359125fbe256616a2dbe68747c1b953f115357
Carl Bäckström <genzorg@gmail.com>
2013-12-23 14:49:09 +0100

    Monitorhost script reads hosts from file and outputs colors.

--- scripts/monitorhost.sh
+++ scripts/monitorhost.sh
@@ -2,6 +2,19 @@
 
 . ~/dotfiles/bashrc.d/global.sh
 
-HOST=burken
-isHostOnline $HOST
-echo test
+hostsfile=~/scripts/hosts
+green='\e[0;32m' # '\e[1;32m' is too bright for white bg.
+red='\e[1;31m'
+endColor='\e[0m'
+
+
+for host in $(cat ${hostsfile})
+do
+  if isHostOnline $host; then
+    echo -e "${green}${host}${endColor}"
+  else    
+    echo -e "${red}$host${endColor}"
+  fi
+done
+
+#$(ping ping.birdstep.com | egrep -o '([1-9]{1,3}\.){3}[1-9]{1,3}')$

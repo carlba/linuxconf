6dda472b89444cefceb24c48c8f0c3f3d4cbba68
Carl Backstrom <carl.backstrom@birdstep.com>
2016-03-23 14:29:05 +0100

    Cron init entry for cleaning vim temporary file in $HOME every minut

--- install/cron.sh
+++ install/cron.sh
@@ -1,1 +1,2 @@
 (crontab -l 2>/dev/null; echo '* 23 * * *  /bin/rm -rf $HOME/temp/*') | crontab -
+(crontab -l 2>/dev/null; echo '* * * * *  /usr/bin/find $HOME -name *~ -exec -rm -rf {} \;') | crontab -

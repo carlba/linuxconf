98903b4ae03081b8a8c50803ef0819def0c259bd
Carl Backstrom <genzorg@gmail.com>
2016-03-24 00:25:23 +0100

    Sync script support two way sync

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,10 +2,23 @@
 
 #root_sync_path="/media/$(whoami)/DATA/backup"
 root_sync_path="$HOME/Dropbox/transfer"
+rsync_command="rsync -avd"
 
 mkdir -p $root_sync_path/bsdev
 
-rsync -av /home/cada/bsdev/gfk-http-collector $root_sync_path/bsdev --exclude venv
-rsync -av /home/cada/bsdev/analytics4 $root_sync_path/bsdev --exclude venv
-rsync -av /home/cada/.PyCharm50 $root_sync_path
+if [[ "$#" -eq 0 ]]; then
+  echo "No arguments provided"
+  exit 1
+fi
 
+if [[ "$1" == "to" ]]; then
+    $rsync_command $HOME/bsdev/gfk-http-collector $root_sync_path/bsdev --exclude venv
+    $rsync_command $HOME/bsdev/analytics4 $root_sync_path/bsdev --exclude venv
+    $rsync_command $HOME/.PyCharm50 $root_sync_path
+fi
+
+if [[ "$1" == "from" ]];then
+    $rsync_command $root_sync_path/bsdev/gfk-http-collector $HOME/bsdev  --exclude venv
+    $rsync_command $root_sync_path/bsdev/analytics4 $HOME/bsdev --exclude venv
+    $rsync_command $root_sync_path/.PyCharm50 $HOME
+fi

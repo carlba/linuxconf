ba8ffdbe93c92032dba5280074c8fc748007dd5a
Carl Backstrom <genzorg@gmail.com>
2016-03-27 08:51:33 +0200

    Added correct path

--- scripts/set_vagrant_hosts.sh
+++ scripts/set_vagrant_hosts.sh
@@ -12,6 +12,6 @@ do
   $HOME/scripts/add_host.sh $hostname.vagrant.dev $ip_address > /dev/null
   ssh-keygen -f "/home/cada/.ssh/known_hosts" -R $hostname.vagrant.dev >/dev/null
   ssh-keyscan -H $hostname.vagrant.dev >> ~/.ssh/known_hosts 2>/dev/null
-  sudo python remove_duplicates.py -o /etc/hosts /etc/hosts
+  sudo python $DOTFILES/scripts/remove_duplicates.py -o /etc/hosts /etc/hosts
 done
 

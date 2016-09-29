10df5534663d150286e24f3eea17e46595480cb8
Carl Backstrom <cbackstrom@smithmicro.com>
2016-09-29 20:26:52 +0200

    Added cadathink to xranderize script

--- scripts/xranderize.sh
+++ scripts/xranderize.sh
@@ -3,4 +3,7 @@
 if [ "$(hostname)" = "uburken" ]
   then
     xrandr --output DVI-I-1 --auto  --rotate normal  --dpi 65 --output HDMI-1 --auto --rotate left --primary --right-of DVI-I-1  --dpi 65
+elif [ "$(hostname)" = "cadathink" ]
+  then
+    echo "test"
 fi

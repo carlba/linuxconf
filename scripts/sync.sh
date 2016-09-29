882ecfa9f7c92e340c097dce41c9d047fb2de779
Carl Backstrom <cbackstrom@smithmicro.com>
2016-09-29 20:03:13 +0200

    Folder in $HOME containing .sync files are synced

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,50 +2,40 @@
 
 #root_sync_path="/media/$(whoami)/DATA/backup"
 
+sync_path="Dropbox/transfer"
 root_sync_path="$HOME/Dropbox/transfer"
 rsync_command="rsync -ad --delete"
-
+find_command=""
 
 function rsync_from_home_to_syncpath {
     mkdir -p $(dirname "$root_sync_path/$1")
     echo -e "\nSyncing $HOME/$1/ with $root_sync_path/$1/"
-    $rsync_command $HOME/$1/ $root_sync_path/$1/ --exclude venv
+    ${rsync_command} $HOME/$1/ ${root_sync_path}/$1/ --exclude venv
 }
 
 function rsync_from_syncpath_to_home {
     mkdir -p $(dirname "$HOME/$1")
     echo -e "\nSyncing $root_sync_path/$1/ with $HOME/$1/"
-    $rsync_command "$root_sync_path/$1/" "$HOME/$1/" --exclude venv
+    ${rsync_command} "$root_sync_path/$1/" "$HOME/$1/" --exclude venv
 }
 
-mkdir -p $root_sync_path/bsdev
+mkdir -p ${root_sync_path}/bsdev
 
 if [[ "$#" -eq 0 ]]; then
   echo "No arguments provided"
   exit 1
 fi
 
-[[ ! -d "$root_sync_path" ]] && echo "$root_sync_path doesn't exist. Dropbox is probably not installed" && exit 1 
+[[ ! -d "$root_sync_path" ]] && echo "$root_sync_path doesn't exist. \
+    Dropbox is probably not installed" && exit 1
 
 if [[ "$1" == "to" ]]; then
-    rsync_from_home_to_syncpath "bsdev/analytics4"
-    rsync_from_home_to_syncpath "bsdev/credentials"
-    rsync_from_home_to_syncpath "bsdev/gfk-http-collector"    
-    rsync_from_home_to_syncpath ".PyCharm2016.1"
-    rsync_from_home_to_syncpath ".gconf/apps/guake"
-    rsync_from_home_to_syncpath ".config/doublecmd"
-    rsync_from_home_to_syncpath ".config/kupfer"
-    rsync_from_home_to_syncpath ".smartgit"
-    rsync_from_home_to_syncpath "docker"
+    find $HOME -name ".sync" -not -path "${root_sync_path}/*" -printf "%P\n" |
+    while read file; do rsync_from_home_to_syncpath $(dirname ${file}); done
 fi
 
 if [[ "$1" == "from" ]];then
-    rsync_from_syncpath_to_home "bsdev/analytics4"
-    rsync_from_syncpath_to_home "bsdev/credentials"
-    rsync_from_syncpath_to_home "bsdev/gfk-http-collector"    
-    rsync_from_syncpath_to_home ".PyCharm2016.1"
-    rsync_from_syncpath_to_home ".gconf/apps/guake"
-    rsync_from_syncpath_to_home ".config/doublecmd/"
-    rsync_from_syncpath_to_home ".config/kupfer"
-    rsync_from_syncpath_to_home "docker"
+    find $HOME -name ".sync" -printf "%P\n" |
+    while read file; do rsync_from_syncpath_to_home $(dirname ${file}); done
+
 fi

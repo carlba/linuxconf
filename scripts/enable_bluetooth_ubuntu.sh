b4bdcbb1836a18858171eae2046c7477c31b3573
Carl Backstrom <cbackstrom@smithmicro.com>
2016-10-10 16:21:25 +0200

    Added sleep to enable bluetooth script

--- scripts/enable_bluetooth_ubuntu.sh
+++ scripts/enable_bluetooth_ubuntu.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-/usr/sbin/rfkill unblock bluetooth
 sudo su -c "echo 1 > /sys/devices/platform/thinkpad_acpi/bluetooth_enable"
+/usr/sbin/rfkill unblock bluetooth
+sleep 2
 sudo hciconfig hci0 up
 sudo hciconfig hci0 piscan

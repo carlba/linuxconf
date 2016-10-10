0fbfd6d14b39aaf7cd07fbfc14d1e9ce1de617c4
Carl Backstrom <cbackstrom@smithmicro.com>
2016-10-10 16:01:30 +0200

    Corrected the screen order of the xranderize.sh

--- scripts/xranderize.sh
+++ scripts/xranderize.sh
@@ -5,5 +5,5 @@ if [ "$(hostname)" = "uburken" ]
     xrandr --output DVI-I-1 --auto  --rotate normal  --dpi 65 --output HDMI-1 --auto --rotate left --primary --right-of DVI-I-1  --dpi 65
 elif [ "$(hostname)" = "cadathink" ]
   then
-    xrandr --output DP2 --auto  --rotate normal  --dpi 63 --output HDMI3 --auto --rotate left --primary --right-of DP2  --dpi 63 --output LVDS1 --off
+    xrandr --output DP2 --auto  --primary --rotate normal  --dpi 63 --output HDMI3 --auto --rotate left --primary --right-of DP2  --dpi 63 --output LVDS1 --off
 fi

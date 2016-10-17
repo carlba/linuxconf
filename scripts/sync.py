02f817ac36fcbb99bab3378c6bed87cf1f15e68b
Carl Backstrom <genzorg@gmail.com>
2016-10-17 22:50:19 +0200

    Updated sync script

--- scripts/sync.py
+++ scripts/sync.py
@@ -1,28 +1,20 @@
 #!/usr/bin/env python2.7
 
 # coding=utf-8
-import os
 
 import click
 from click.testing import CliRunner
-# noinspection PyUnresolvedReferences
-from sh import locate, rsync, updatedb
+from scandir import  walk
+from sh import rsync
 from pathlib2 import Path
-
 
 # http://click.pocoo.org/5/commands/
 # https://pathlib.readthedocs.io/en/pep428/
 # Coloring output http://click.pocoo.org/5/utils/
 
 root_sync_path = "/home/cada/Dropbox/transfer"
-rsync_command = "rsync -adn --delete"
-
-if not os.geteuid() == 0:
-    click.echo(click.style('The mlocate database will not be updated since the script is not '
-                           'running as root.', blink=True, bold=True))
-else:
-    updatedb()
-
+excludes = {'/media/Windows/Users/genzo/Dropbox/transfer', '.cache', 'VirtualBox VMs',
+            'Downloads', '.vagrant.d', '.dropbox', 'venv', 'Videos', 'Dropbox'}
 
 @click.group()
 def cli():
@@ -32,13 +24,22 @@ def cli():
 def _sync(sync_mappings):
     for from_path, to_path in sync_mappings:
         to_path.mkdir(parents=True, exist_ok=True)
+
+        exclude_params = zip(len(excludes) * ['--exclude'], excludes)
+
         rsync('-ad', from_path.as_posix() + '/', to_path.as_posix() + '/',
-              delete=True, exclude='venv')
+              delete=True, *exclude_params)
 
 
 def _print_sync_mappings(sync_mappings):
     for from_path, to_path in sync_mappings:
         click.echo('{} -> {}'.format(from_path, to_path))
+
+
+def _get_sync_paths(path, excludes):
+    for root, dirs, files in walk(path):
+        if '.sync' in files and not any(x in root for x in excludes):
+            yield Path(root)
 
 
 @cli.command()
@@ -48,14 +49,11 @@ def store():
     ``root_sync_path``
 
     """
-    sync_paths = [Path(item).parent for item in locate('*.sync').rstrip('\n').split('\n')
-                  if not item.startswith(root_sync_path)]
 
+    sync_paths = _get_sync_paths('/home/cada', excludes | {root_sync_path})
     sync_mappings = [(path, Path(root_sync_path) / path.relative_to('/'))
                      for path in sync_paths]
-
     _print_sync_mappings(sync_mappings)
-
     _sync(sync_mappings)
 
 
@@ -65,21 +63,14 @@ def load():
     Loads all directories in the ``root_sync_path`` that contains a .sync file to their
     matching path in the filesystem
     """
-    sync_paths = [Path(item).parent for item in locate('*.sync').rstrip('\n').split('\n')
-                  if item.startswith(root_sync_path)]
 
+    sync_paths = _get_sync_paths(root_sync_path, excludes)
     sync_mappings = [(path, Path('/') / path.relative_to(root_sync_path))
                      for path in sync_paths]
-
     _print_sync_mappings(sync_mappings)
-
     _sync(sync_mappings)
+
 
 if __name__ == '__main__':
     cli()
     runner = CliRunner()
-
-
-
-
-

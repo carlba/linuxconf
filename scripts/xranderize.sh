5b2fc3124b6a0fcf28f8aa897332ce694272abe5
Carl Backstrom <genzorg@gmail.com>
2016-10-19 22:00:18 +0200

    Changes to xranderize.sh

--- scripts/xranderize.sh
+++ scripts/xranderize.sh
@@ -2,8 +2,8 @@
 
 if [ "$(hostname)" = "uburken" ]
   then
-    xrandr --output DVI-I-1 --auto  --rotate normal  --dpi 65 --output HDMI-1 --auto --rotate left --primary --right-of DVI-I-1  --dpi 65
+    xrandr --output DVI-I-1 --auto  --primary --dpi 84 --rotate normal --output HDMI-1 --auto --dpi 84 --rotate left --right-of DVI-I-1 
 elif [ "$(hostname)" = "cadathink" ]
   then
-    xrandr --output DP2 --auto  --primary --rotate normal  --dpi 63 --output HDMI3 --auto --rotate left --primary --right-of DP2  --dpi 63 --output LVDS1 --off
+    xrandr --output DP2 --auto  --primary --rotate normal  --dpi 70 --output HDMI3 --auto --rotate left --primary --right-of DP2  --dpi 70 --output LVDS1 --off
 fi
